ec7c92ebb914e708ffcbe564268427b0320199ef
Zofia Drabek <zosia.drabek@gmail.com>
2019-06-25 15:45:01 +0200

    set up in interface builder

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "add", sender: sender)
+    }
+    
 }
 
